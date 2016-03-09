31cd813a8a6e0276694686b629cbad38a4c9fc77
Matthew Turner <maturner01@gmail.com>
2016-03-09 12:09:11 -0800

    added shear mesh-to-casimir ascii code

--- jemez/dflow_casimir.py
+++ jemez/dflow_casimir.py
@@ -76,6 +76,51 @@ def casimir(vegetation_map, shear_map, shear_resistance_dict):
     return ret_veg_map
 
 
+def shear_mesh_to_asc(dflow_out_nc_path, west_easting_val, n_eastings,
+                      south_northing_val, n_northings, cellsize):
+    """
+    Extract flow element values and locations from the dflow output netcdf
+    and project these onto the grid defined by the corner of the
+    grid defined by the lower-left corner of the bounding box and the
+    cell size. The results are saved in ESRI .asc format to asc_out_path.
+
+    Arguments:
+        dflow_out_nc_path (str): location of the dflow netcdf on disk
+        west_easting_val (float): lower-left corner easting
+        south_northing_val (float): lower-left corner northing
+        cellsize (float): resolution of the output grid. probably determined
+            by the input vegetation map .asc
+
+    Returns:
+        (ESRIAsc) representation of gridded representation of the mesh shear
+            stress data output from dflow
+    """
+    dflow_ds = open_dataset(dflow_out_nc_path)
+
+    # the mesh locations are the x and y centers of the Flow (Finite) Elements
+    mesh_x = dflow_ds.FlowElem_xcc
+    mesh_y = dflow_ds.FlowElem_ycc
+    mesh_shear = dflow_ds.taus[-1]  # take the last timestep
+
+    x = array([west_easting_val + (i*cellsize) for i in range(n_eastings)])
+
+    y = array([south_northing_val + (i*cellsize) for i in range(n_northings)])
+
+    grid_x, grid_y = meshgrid(x, y)
+
+    # use linear interp so we don't have to install natgrid
+    asc_mat = griddata((mesh_x, mesh_y), mesh_shear, (grid_x, grid_y))
+
+    # not sure why, but this makes it align with the original vegetation map
+    asc_mat = flipud(asc_mat)
+
+    data = Series(reshape(asc_mat, (n_eastings * n_northings)))
+
+    return ESRIAsc(ncols=n_eastings, nrows=n_northings,
+                   xllcorner=west_easting_val, yllcorner=south_northing_val,
+                   cellsize=cellsize, data=data)
+
+
 class ESRIAsc:
 
     def __init__(self, file_path=None, ncols=None, nrows=None,
