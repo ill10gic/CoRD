083569195e9d08d4bf44ad426a859821b1d9c7d1
Matthew Turner <maturner01@gmail.com>
2016-08-16 14:20:44 -0700

    fixed wall time; 0.2.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = [
 setup(
     name='cord',
     description='Coupled RipCAS-DFLOW for Vegetation and Hydrology of Streams',
-    version='0.2.1',
+    version='0.2.3',
     author_email='maturner01@gmail.com',
     author='Matthew Turner',
     license='BSD3',
