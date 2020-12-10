ddc46a35c273d1f8f1f49d529b6035c663e2ac32
Stephen Moser <42612190+ill10gic@users.noreply.github.com>
2020-12-09 20:47:02 -0700

    Bugfix/continue cord (#5)
    * adding to git ignore
    
    * Cord appears to be running in debug mode handling the flow threshold, with specified partition file location. Continuation also appears to be working fine
    
    * fixes some issue with the continue cord cord that was causing it to get incorrect values
    
    * fixes broken comment
    
    Co-authored-by: Stephen Moser <xxxxxx>

--- cord/scripts/cord.py
+++ cord/scripts/cord.py
@@ -164,13 +164,14 @@ def _run_ripcas(dflow_run_dir='/users/maturner/partition-run-dev'):
 @click.pass_context
 def from_config(ctx, config_file, continue_cord):
     """Run CoRD with params from <config_file>"""
-
+    # input('Press a button to continue')
     cfg = load_args_from_config(config_file)
     ctxlog = ctx.obj['LOGFILE']
     logfile = ctxlog if ctxlog is not None else cfg['log_f']
     progressfile = 'cord_progress.log'
     modelrun_series(
         cfg['data_dir'],
+        cfg['partitioned_inputs_dir'],
         cfg['initial_vegetation_map'], #vegclass_2z.asc - default
         cfg['vegzone_map'], #zonemap_2z.asc  - default
         cfg['veg_roughness_shearres_lookup'], #veg_roughness_shearres.xlsx  - default
@@ -379,6 +380,9 @@ def load_args_from_config(config_file):
         gen['geometry_file'] = os.path.join(
             curdir, '..', 'data', 'dflow_inputs', 'DBC_geometry.xyz'
         )
+
+    if gen['partitioned_inputs_dir'] == u'':
+        raise RuntimeError('partitioned_inputs_dir must be defined in ' + config_file)
 
     if gen['peak_flows_file'] == u'':
         raise RuntimeError('PEAK_FLOWS_FILE must be defined in ' + config_file)
